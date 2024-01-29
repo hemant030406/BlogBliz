ab1172129a19c2a3ee02e5e70953ab7157de41e8
hemant030406 <112201024@smail.iitpkd.ac.in>
2024-01-29 10:42:49 +0530

    new

--- src/components/JS/About.js
+++ src/components/JS/About.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 import '../CSS/About.css'
 
-const About = ()=>{
-    const [about,setAbout] = useState(undefined)
+const About = () => {
+    const [about, setAbout] = useState(undefined)
     const fetchAbout = async () => {
-        setAbout(await fetch('https://webmosaic.petrichor.events/about').then(res=>res.json()).then(data => data.about))
+        setAbout(await fetch('https://webmosaic.petrichor.events/about')
+            .then(res => res.json())
+            .then(data => data.about))
         console.log(about)
     }
-    if(!about){
+    if (!about) {
         fetchAbout()
     }
     return (
         <div className="about">
-        {about}
+            {about}
         </div>
     )
 }
